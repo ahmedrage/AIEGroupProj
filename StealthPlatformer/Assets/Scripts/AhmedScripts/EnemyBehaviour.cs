b4d43a6f1a218191625267750cc32c9ddcbb6d6d
ahmedrage <ahmedrage@hotmail.com>
2016-08-25 18:03:03 +0930

    Added basic functionality to enemy patrolling

---
+++ StealthPlatformer/Assets/Scripts/AhmedScripts/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public Rigidbody2D m_rigidbody2d; //Enemy rigidbody
+	public enum state // Defining the enemy states
+	{
+		Patrolling,
+		Alert,
+		Persuing
+	};
+	public float m_Speed;
+	public int direction = 1;
+
+	public Transform[] patrolPoints;
+
+	public state enemyState; // The current state of the enemy
+
+	void Start () {
+		m_rigidbody2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	void Update () {
+		if (enemyState == state.Patrolling) {
+			if (transform.position.x >= patrolPoints [0].position.x) {
+				direction = -1;
+			} else if (transform.position.x <= patrolPoints[1].position.x) {
+				direction = 1;
+			}
+
+			m_rigidbody2d.velocity = Vector2.right * direction * m_Speed;
+		
+		}
+	}
+}
