00268c9a4c047e930f0503824ca67f00297df594
ahmedrage <ahmedrage@hotmail.com>
2016-09-13 19:09:17 +0930

    Added hiding place

---
+++ StealthPlatformer/Assets/Scripts/AhmedScripts/hidingSpot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hidingSpot : MonoBehaviour {
+	public int playerLayerIndex = 9;
+	public int hidingLayerIndex = 10;
+	public Color hidingColor;
+	public SpriteRenderer m_spriteRenderer;
+
+	Color initialColor;
+	// Use this for initialization
+	void Start () {
+		m_spriteRenderer = GetComponent<SpriteRenderer> ();
+		initialColor = m_spriteRenderer.color;
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.layer == playerLayerIndex) {   
+			m_spriteRenderer.color = hidingColor;
+			other.gameObject.layer = hidingLayerIndex;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		if (other.gameObject.layer == hidingLayerIndex) {
+			m_spriteRenderer.color = initialColor;
+			other.gameObject.layer = playerLayerIndex;
+		}
+	}
+}
